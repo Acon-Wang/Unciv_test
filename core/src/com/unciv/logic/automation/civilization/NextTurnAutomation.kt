aec1fdbd2d6402b64a5a80bc03a864f3cdf33026
Acon-Wang <2412500114@qq.com>
2024-04-12 18:33:21 +0800

    在DebugUtils中添加开关（需要用大模型接入的文明）

--- core/src/com/unciv/logic/automation/civilization/NextTurnAutomation.kt
+++ core/src/com/unciv/logic/automation/civilization/NextTurnAutomation.kt
@@ -161,7 +161,7 @@ object NextTurnAutomation {
     private fun respondToPopupAlerts(civInfo: Civilization) {
         val content = UncivFiles.gameInfoToString(civInfo.gameInfo,false,false)
         ///该回合请求使用哪几种技能
-        if (civInfo.gameInfo.turns % 5 == 0) {
+        if (civInfo.gameInfo.turns % 5 == 0&&DebugUtils.NEED_LLM_NAME==civInfo.civName) {
             val contentData = ContentData_two(content, civInfo.civName)
             val jsonString = Json.encodeToString(contentData)
             val postRequestResult = sendPostRequest("http://127.0.0.1:2337/use_tools", jsonString)
